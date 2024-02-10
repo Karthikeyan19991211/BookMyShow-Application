a840752600cc81d149145e99679b0de2fa0d62f5
Karthikeyan19991211 <karthisubramaniyan1999@gmail.com>
2024-02-10 11:01:31 +0530

    BookMyShow Application hsa been created

---
+++ src/test/java/com/org/project/BookMyShow/BookMyShowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.project.BookMyShow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyShowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
